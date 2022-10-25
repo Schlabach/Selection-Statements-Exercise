802b7d793c209bd5e4dcb11301b82724a998fdef
Schlabach <matthewshawn89@gmail.com>
2022-10-25 12:39:48 -0400

    Initial Commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess my favorite number");
+
+            var r = new Random();
+            var favNumber = r.Next(1,1000);
+            var userInput = int.Parse(Console.ReadLine());	
+			
+			
+                if(userInput < favNumber)
+                {
+                    Console.WriteLine($"Too Low");
+                }
+                else if(userInput == favNumber)
+                {
+                    Console.WriteLine($"Correct!");
+                }
+                else if(userInput > favNumber)
+                {
+                    Console.WriteLine($"Too High");
+                }
+                else
+                {
+                    Console.WriteLine($"Nevermind");
+                }
         }
     }
 }
